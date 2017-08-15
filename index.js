b96a8be072aee683b72c8396f266494873b6493c
longntx <long.ntx@gmail.com>
2017-08-15 20:38:58 +0700

    Fix compatibility node v7

--- index.js
+++ index.js
@@ -1,69 +1,104 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Marketplace
+Gist
+ @longntx
+ Sign out
+ Watch 1
+  Star 0
+ Fork 4 ra100/karma-jenkins-reporter
+forked from actano/karma-jenkins-reporter
+ Code  Pull requests 0  Projects 0  Wiki Insights 
+Tree: 834824be92 Find file Copy pathkarma-jenkins-reporter/index.js
+834824b  on Aug 30, 2016
+@ra100 ra100 Fix issue #2 compatibility with node v6+
+6 contributors @vojtajina @timaschew @treyhyde @sylvain-hamel @ra100 @eventualbuddha
+RawBlameHistory      
+157 lines (129 sloc)  4.05 KB
 var os = require('os');
 var path = require('path');
 var fs = require('fs');
 var builder = require('xmlbuilder');
 
+// concatenate test suite(s) and test description by default
+function defaultNameFormatter(browser, result) {
+  return result.suite.join(' ') + ' ' + result.description
+}
 
-var JenkinsReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
+var JenkinsReporter = function (baseReporterDecorator, config, logger, helper, formatError) {
   var log = logger.create('reporter.jenkins');
   var reporterConfig = config.jenkinsReporter || {};
   var pkgName = reporterConfig.suite || '';
-  var featurePath = process.env.REPORT_FILE;
-  var outputFile = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputFile
-      || 'test-results.xml'));
-  if (featurePath != null) {
-    outputFile = path.join(process.env.PREFIX || '', featurePath);
+  var outputFile = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputFile || 'test-results.xml'));
+  var useBrowserName = reporterConfig.userBrowserName
+  if (typeof useBrowserName === 'undefined') {
+    useBrowserName = true
   }
+
   var xml;
   var suites;
   var pendingFileWritings = 0;
-  var fileWritingFinished = function() {};
+  var fileWritingFinished = function () {};
   var allMessages = [];
 
   baseReporterDecorator(this);
 
-  this.adapters = [function(msg) {
-    allMessages.push(msg);
-  }];
+  this.adapters = [function (msg) {
+      allMessages.push(msg);
+    }
+  ];
 
-  var initliazeXmlForBrowser = function(browser) {
+  var initializeXmlForBrowser = function (browser) {
     var timestamp = (new Date()).toISOString().substr(0, 19);
     var suite = suites[browser.id] = xml.ele('testsuite', {
-      name: browser.name, 
-      'package': pkgName, 
-      timestamp: timestamp, 
-      id: 0, 
-      hostname: os.hostname(),
-      make_target: process.env.MAKE_TARGET
+      'name': browser.name,
+      'package': pkgName,
+      'timestamp': timestamp,
+      'id': 0,
+      'hostname': os.hostname(),
+      'make_target': process.env.MAKE_TARGET
+    })
+
+    suite.ele('properties').ele('property', {
+      name: 'browser.fullName',
+      value: browser.fullName
     });
-    suite.ele('properties').ele('property', {name: 'browser.fullName', value: browser.fullName});
   };
 
-  this.onRunStart = function(browsers) {
+  var getClassName = function (browser, result) {
+    var browserName = browser.name.replace(/ /g, '_').replace(/\./g, '_') + '.'
+
+    return (useBrowserName
+      ? browserName
+      : '') + (pkgName
+      ? pkgName + '.'
+      : '') + result.suite[0]
+  }
+
+  this.onRunStart = function (browsers) {
     suites = Object.create(null);
     xml = builder.create('testsuites');
-
-    // TODO(vojta): remove once we don't care about Karma 0.10
-    browsers.forEach(initliazeXmlForBrowser);
   };
 
-  this.onBrowserStart = function(browser) {
-    initliazeXmlForBrowser(browser);
+  this.onBrowserStart = function (browser) {
+    initializeXmlForBrowser(browser);
   };
 
-  this.onBrowserComplete = function(browser) {
+  this.onBrowserComplete = function (browser) {
     var suite = suites[browser.id];
-
-    if (!suite) {
-      // This browser did not signal `onBrowserStart`. That happens
-      // if the browser timed out duging the start phase.
-      return;
-    }
-
     var result = browser.lastResult;
 
+    if (!suite || !result) {
+      return // don't die if browser didn't start
+    }
+
     suite.att('tests', result.total);
-    suite.att('errors', result.disconnected || result.error ? 1 : 0);
+    suite.att('errors', result.disconnected || result.error
+      ? 1
+      : 0);
     suite.att('failures', result.failed);
     suite.att('time', (result.netTime || 0) / 1000);
 
@@ -71,12 +106,12 @@ var JenkinsReporter = function(baseReporterDecorator, config, logger, helper, fo
     suite.ele('system-err');
   };
 
-  this.onRunComplete = function() {
+  this.onRunComplete = function () {
     var xmlToOutput = xml;
 
     pendingFileWritings++;
-    helper.mkdirIfNotExists(path.dirname(outputFile), function() {
-      fs.writeFile(outputFile, xmlToOutput.end({pretty: true}), function(err) {
+    helper.mkdirIfNotExists(path.dirname(outputFile), function () {
+      fs.writeFile(outputFile, xmlToOutput.end({pretty: true}), function (err) {
         if (err) {
           log.warn('Cannot write xml\n\t' + err.message);
         } else {
@@ -93,30 +128,38 @@ var JenkinsReporter = function(baseReporterDecorator, config, logger, helper, fo
     allMessages.length = 0;
   };
 
-  this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
-    var classnameArray = path.dirname(featurePath).split('/');
-    if(reporterConfig.classnameSuffix != null) classnameArray.push(reporterConfig.classnameSuffix);
+  this.specSuccess = this.specSkipped = this.specFailure = function (browser, result) {
+    var testsuite = suites[browser.id]
 
-    var spec = suites[browser.id].ele('testcase', {
-      name: result.description, time: ((result.time || 0) / 1000),
-      classname: classnameArray.join('.'),
-      'package': (pkgName ? pkgName + ' ' : '') + browser.name,
+    if (!testsuite) {
+      return
+    }
+
+    var spec = testsuite.ele('testcase', {
+      name: defaultNameFormatter(browser, result),
+      time: ((result.time || 0) / 1000),
+      classname: getClassName(browser, result),
+      package: (pkgName
+        ? pkgName + ' '
+        : '') + browser.name,
       parentSuites: result.suite.join('|')
-    });
+    })
 
     if (result.skipped) {
-      spec.ele('skipped');
+      spec.ele('skipped')
     }
 
     if (!result.success) {
-      result.log.forEach(function(err) {
-        spec.ele('failure', {type: ''}, formatError(err));
-      });
+      result.log.forEach(function (err) {
+        spec.ele('failure', {
+          type: ''
+        }, formatError(err))
+      })
     }
   };
 
   // wait for writing all the xml files, before exiting
-  this.onExit = function(done) {
+  this.onExit = function (done) {
     if (pendingFileWritings) {
       fileWritingFinished = done;
     } else {
@@ -131,3 +174,5 @@ JenkinsReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper'
 module.exports = {
   'reporter:jenkins': ['type', JenkinsReporter]
 };
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
